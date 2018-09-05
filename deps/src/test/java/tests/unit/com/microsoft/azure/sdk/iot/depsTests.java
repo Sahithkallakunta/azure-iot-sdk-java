9968f5b6eb5610b4f53359cb3504cc68a00aa823
timtay-microsoft <timtay@microsoft.com>
2018-09-05 16:27:16 -0700

    added a test

---
+++ deps/src/test/java/tests/unit/com/microsoft/azure/sdk/iot/depsTests.java
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) Microsoft. All rights reserved.
+ *  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+package tests.unit.com.microsoft.azure.sdk.iot;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class depsTests
+{
+    @Test
+    public void someTest()
+    {
+        System.out.println("I'm a test!");
+        assertTrue(true);
+    }
+}
