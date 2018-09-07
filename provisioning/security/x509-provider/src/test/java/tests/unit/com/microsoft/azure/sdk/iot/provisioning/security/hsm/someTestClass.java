b1bbb931857f8457ac106e70373aeee4fd48ed99
timtay-microsoft <timtay@microsoft.com>
2018-09-07 11:38:02 -0700

    does the test matter?

--- provisioning/security/x509-provider/src/test/java/tests/unit/com/microsoft/azure/sdk/iot/provisioning/security/hsm/someTestClass.java
+++ provisioning/security/x509-provider/src/test/java/tests/unit/com/microsoft/azure/sdk/iot/provisioning/security/hsm/someTestClass.java
@@ -1,19 +1,46 @@
-/*
- *  Copyright (c) Microsoft. All rights reserved.
- *  Licensed under the MIT license. See LICENSE file in the project root for full license information.
- */
+ /*
+  *  Copyright (c) Microsoft. All rights reserved.
+  *  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+  */
 
-package tests.unit.com.microsoft.azure.sdk.iot.provisioning.security.hsm;
 
-import org.junit.Ignore;
 
-import static junit.framework.TestCase.fail;
+ package tests.unit.com.microsoft.azure.sdk.iot.provisioning.security.hsm;
 
-public class someTestClass
-{
-    @Ignore
-    public void supposedToFail()
-    {
-        fail("This test is supposed to fail");
-    }
-}
+ import org.junit.Test;
+
+ import static junit.framework.TestCase.assertTrue;
+ import static junit.framework.TestCase.fail;
+
+ public class someTestClass
+ {
+     @Test
+     public void someTest()
+     {
+         System.out.println("I'm a test!");
+         assertTrue(true);
+     }
+
+     @Test
+     public void evenMoreTests()
+     {
+         System.out.println("The merge over PR worked!");
+         assertTrue(true);
+     }
+
+     @Test
+     public void supposedToFail()
+     {
+         fail("This test is supposed to fail");
+     }
+
+     @Test
+     public void flakeyTest()
+     {
+         double a = Math.floor(Math.random()*4);
+         if (a >= 3)
+         {
+             fail("Bad luck, fella");
+         }
+     }
+ }
---
+++ provisioning/security/x509-provider/src/test/java/tests/unit/com/microsoft/azure/sdk/iot/provisioning/security/hsm/someTestClassTest.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) Microsoft. All rights reserved.
+ *  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+package tests.unit.com.microsoft.azure.sdk.iot.provisioning.security.hsm;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.fail;
+
+public class someTestClassTest
+{
+    @Test
+    public void someTest()
+    {
+        System.out.println("I'm a test!");
+        assertTrue(true);
+    }
+
+    @Test
+    public void evenMoreTests()
+    {
+        System.out.println("The merge over PR worked!");
+        assertTrue(true);
+    }
+
+    @Test
+    public void supposedToFail()
+    {
+        fail("This test is supposed to fail");
+    }
+
+    @Test
+    public void flakeyTest()
+    {
+        double a = Math.floor(Math.random()*4);
+        if (a >= 3)
+        {
+            fail("Bad luck, fella");
+        }
+    }
+}
